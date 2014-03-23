3551ecfb566e59bb8f8005b7a76891ad8ca6acb7
Nigeriacoin <contact@nigeriacoin.org>
2014-03-23 03:04:19 +0100

    updated difficulty

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -204,7 +204,7 @@ nets = dict(
         BLOCK_EXPLORER_URL_PREFIX='https://explorer.nigeriacoin.org/block/',
         ADDRESS_EXPLORER_URL_PREFIX='https://explorer.nigeriacoin.org/address/',
         TX_EXPLORER_URL_PREFIX='https://explorer.nigeriacoin.org/tx/',
-        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**32 - 1),
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.01e8,
     )
