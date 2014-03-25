1701c63e3acfefad9ab14e4693c4679977ff4026
Nigeriacoin <contact@nigeriacoin.org>
2014-03-25 17:26:09 +0100

    fixed max target and added skeincoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -207,6 +207,27 @@ nets = dict(
         SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.01e8,
+    ),
+    skeincoin=math.Object(
+        P2P_PREFIX='f726a1bf'.decode('hex'),
+        P2P_PORT=11230,
+        ADDRESS_VERSION=63,
+        RPC_PORT=21230,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            "skeincoinaddress" in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 10000 if height < 100 else max((32*int(1e8))>> (height + 1)//262800, 10000),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('skeinhash').getPoWHash(data)),
+        BLOCK_PERIOD=120, # s
+        SYMBOL='SKC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Skeincoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Skeincoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.skeincoin'), 'skeincoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='https://explorer.skeincoin.org/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='https://explorer.skeincoin.org/address/',
+        TX_EXPLORER_URL_PREFIX='https://explorer.skeincoin.org/tx/',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=1e8,
     )
 
 )
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -133,13 +133,32 @@ nets = dict(
         PREFIX='af69020acd611ac0'.decode('hex'),
         P2P_PORT=9557,
         MIN_TARGET=16,
-        MAX_TARGET=2**256//2**32 - 1,
+        MAX_TARGET=2**256//2**20 - 1,
         PERSIST=True,
         WORKER_PORT=9556,
         BOOTSTRAP_ADDRS='p2pool1.nigeriacoin.org p2pool2.nigeriacoin.org'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-ngc',
         VERSION_CHECK=lambda v: v >= 90000,
         VERSION_WARNING=lambda v: 'Upgrade Nigeriacoin to >=0.9.0!' if v < 90000 else None,
+    ),
+    skeincoin=math.Object(
+        PARENT=networks.nets['skeincoin'],
+        SHARE_PERIOD=12, # seconds
+        CHAIN_LENGTH=24*60*60//12, # shares
+        REAL_CHAIN_LENGTH=24*60*60//12, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=15, # blocks
+        IDENTIFIER='f134a62e7cee0cab'.decode('hex'),
+        PREFIX='9030d1a8f32a9d55'.decode('hex'),
+        P2P_PORT=19230,
+        MIN_TARGET=16,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=True,
+        WORKER_PORT=18230,
+        BOOTSTRAP_ADDRS=''.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-skc',
+        VERSION_CHECK=lambda v: v >= 90000,
+        VERSION_WARNING=lambda v: 'Upgrade Skeincoin to >=0.8.6!' if v < 80600 else None,
     )
 )
 for net_name, net in nets.iteritems():
